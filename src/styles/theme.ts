0bef1c22907b52b2d0c75cb21a787d6ae88578dd
TiagoDiass <tiago.costadiasss@gmail.com>
2022-05-29 17:49:19 -0300

    feat: add theme variables

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,32 @@
 const theme = {
   colors: {
-    primary: '#06092b',
-    white: '#fff'
+    primary: {
+      accent1: '#FAFAFA',
+      accent2: '#EAEAEA',
+      accent3: '#999'
+    },
+
+    secondary: {
+      accent1: '#444',
+      accent2: '#333',
+      accent3: '#151515'
+    },
+
+    error: {
+      default: '#FF1A1A',
+      darker: '#E00'
+    },
+
+    success: {
+      lighter: '#3291FF',
+      default: '#0070F3',
+      darker: '#0761D1'
+    },
+
+    warning: {
+      default: '#F5A623',
+      darker: '#AB570A'
+    }
   }
 } as const;
 
