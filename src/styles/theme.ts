f2af789664ec987ae760d2289cb1df864aeb17a0
TiagoDiass <tiago.costadiasss@gmail.com>
2022-05-29 21:54:12 -0300

    feat: add other theme properties

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -27,6 +27,29 @@ const theme = {
       default: '#F5A623',
       darker: '#AB570A'
     }
+  },
+
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  },
+
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out'
   }
 } as const;
 
