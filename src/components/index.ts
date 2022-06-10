11b8ff6e505815150e62f159d0220079ea36405c
Tiago Dias <tiago.costadiasss@gmail.com>
2022-06-10 20:43:55 -0300

    chore: comment

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Main } from './Main/Main';
 export { default as Button } from './Button/Button';
 export { default as TextField } from './TextField/TextField';
 export { default as Breadcrumb } from './Breadcrumb/Breadcrumb';
+// remove me later
