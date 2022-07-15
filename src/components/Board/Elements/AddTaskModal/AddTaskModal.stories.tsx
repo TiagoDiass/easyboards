1aa02d7fe3ffd6a324638ea3f684332f826574c2
TiagoDiass <tiago.costadiasss@gmail.com>
2022-07-14 23:28:52 -0300

    chore: remove AddTaskModal stories

--- src/components/Board/Elements/AddTaskModal/AddTaskModal.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import AddTaskModal from './AddTaskModal';
-
-export default {
-  title: 'AddTaskModal',
-  component: AddTaskModal
-} as ComponentMeta<typeof AddTaskModal>;
-
-export const Basic: ComponentStory<typeof AddTaskModal> = (args) => <AddTaskModal {...args} />;
