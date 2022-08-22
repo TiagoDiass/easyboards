1a82fd9bc791f29485cc6f604d5ce3484f31da40
TiagoDiass <tiago.costadiasss@gmail.com>
2022-08-22 20:02:58 -0300

    test: remove skip flag from useHandleAddTask test

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,2 +1,6 @@
 import '@testing-library/jest-dom';
 import 'jest-styled-components';
+
+beforeAll(() => {
+  global.structuredClone = (value: any) => JSON.parse(JSON.stringify(value));
+});
--- src/components/Board/logic/useHandleAddTask/useHandleAddTask.test.ts
+++ src/components/Board/logic/useHandleAddTask/useHandleAddTask.test.ts
@@ -19,7 +19,7 @@ const BOARD_MOCK: Board = {
   ]
 };
 
-xdescribe('Component: Board > Logic hook: useHandleAddTask', () => {
+describe('Component: Board > Logic hook: useHandleAddTask', () => {
   it('should setBoard correctly and then closeModal', () => {
     // as this hook doesn't use any React logic, we don't need to use @testing-library/react-hooks
     const closeAddTaskModalMock = jest.fn();
@@ -35,7 +35,27 @@ xdescribe('Component: Board > Logic hook: useHandleAddTask', () => {
     handleAddTask('My new task');
 
     expect(setBoardMock).toHaveBeenCalledTimes(1);
-    expect(setBoardMock).toHaveBeenCalledWith(1);
+    expect(setBoardMock).toHaveBeenCalledWith({
+      id: 'fake-id',
+      slug: 'fake-slug',
+      title: 'fake-board',
+      columns: [
+        {
+          id: 'column-1',
+          title: 'Column 1',
+          tasks: [
+            {
+              id: 'fake-task-1',
+              content: 'Fake first task'
+            },
+            {
+              id: expect.any(String),
+              content: 'My new task'
+            }
+          ]
+        }
+      ]
+    });
     expect(closeAddTaskModalMock).toHaveBeenCalledTimes(1);
   });
 });
