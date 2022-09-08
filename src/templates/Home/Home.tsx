0d94e958ebbfa906612f460aa236d46c7d37191f
TiagoDiass <tiago.costadiasss@gmail.com>
2022-09-08 20:41:21 -0300

    chore: add title tag on Head on Home page

--- src/templates/Home/Home.tsx
+++ src/templates/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import useBoardStore from 'store/board/board.store';
 import * as S from './Home.styles';
@@ -6,27 +7,35 @@ export default function Home() {
   const boards = useBoardStore((store) => store.state.partialBoards);
 
   return (
-    <S.Wrapper>
-      <S.Title>👋 Hey! Welcome to ReactTrello</S.Title>
+    <>
+      <Head>
+        <title>ReactTrello - Home</title>
+      </Head>
 
-      <S.Description>
-        With this app, you can <strong>easily</strong> manage your tasks by creating specific boards
-        to specific contexts, separating them into columns and task cards.
-      </S.Description>
+      <S.Wrapper>
+        <S.Title>👋 Hey! Welcome to ReactTrello</S.Title>
 
-      <S.SelectBoardWrapper>
-        <span className='select-a-board-text'>Select a board to start managing your tasks 😉</span>
+        <S.Description>
+          With this app, you can <strong>easily</strong> manage your tasks by creating specific
+          boards to specific contexts, separating them into columns and task cards.
+        </S.Description>
 
-        <ul className='boards-list'>
-          {boards.map((board) => (
-            <li className='boards-list-item' key={board.id}>
-              <Link href={`/boards/${board.slug}`}>
-                <a>{board.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </S.SelectBoardWrapper>
-    </S.Wrapper>
+        <S.SelectBoardWrapper>
+          <span className='select-a-board-text'>
+            Select a board to start managing your tasks 😉
+          </span>
+
+          <ul className='boards-list'>
+            {boards.map((board) => (
+              <li className='boards-list-item' key={board.id}>
+                <Link href={`/boards/${board.slug}`}>
+                  <a>{board.title}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </S.SelectBoardWrapper>
+      </S.Wrapper>
+    </>
   );
 }
