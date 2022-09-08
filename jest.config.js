d3747f766e956187c055acbcb8b04855f9234f41
TiagoDiass <tiago.costadiasss@gmail.com>
2022-09-08 20:41:04 -0300

    chore: remove files that dont need to have coverage measured

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,12 @@ module.exports = {
     'src/**/*.ts(x)?',
     '!src/**/*.stories.tsx',
     '!src/components/Board/Board.mock.ts',
-    '!src/constants/initial-board.ts'
+    '!src/constants/initial-board.ts',
+    '!src/pages/*.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/*',
+    '!src/types/*',
+    '!src/logic/useOnDragEnd/useOnDragEnd.ts' // we don't test the drag and drop
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   moduleDirectories: ['node_modules', 'src']
