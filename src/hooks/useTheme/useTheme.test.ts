5944d934742a0a46b012213bc6c7e8d393ca8609
TiagoDiass <tiago.costadiasss@gmail.com>
2023-07-16 12:26:15 -0300

    refactor: remove theme persisting

--- src/hooks/useTheme/useTheme.test.ts
+++ src/hooks/useTheme/useTheme.test.ts
@@ -1,29 +1,15 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 import useTheme from './useTheme';
-import 'jest-localstorage-mock';
 import { waitFor } from '@testing-library/react';
-import { CURRENT_THEME_KEY } from 'constants/local-storage-keys';
 
 describe('Hook: useTheme', () => {
-  it('should get the current theme from local storage', () => {
-    jest.spyOn(localStorage, 'getItem').mockReturnValue('dark');
-
-    const result = renderHook(() => useTheme()).result;
-
-    expect(result.current.currentTheme).toBe('dark');
-  });
-
   it('should use the light theme as default', () => {
-    jest.spyOn(localStorage, 'getItem').mockReturnValue(null);
-
     const result = renderHook(() => useTheme()).result;
 
     expect(result.current.currentTheme).toBe('light');
   });
 
   it('should toggle theme correctly', async () => {
-    jest.spyOn(localStorage, 'getItem').mockReturnValue('dark');
-
     const result = renderHook(() => useTheme()).result;
 
     act(() => {
@@ -31,8 +17,7 @@ describe('Hook: useTheme', () => {
     });
 
     await waitFor(() => {
-      expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-      expect(localStorage.setItem).toHaveBeenCalledWith(CURRENT_THEME_KEY, 'light');
+      expect(result.current.currentTheme).toBe('dark');
     });
 
     act(() => {
@@ -40,8 +25,7 @@ describe('Hook: useTheme', () => {
     });
 
     await waitFor(() => {
-      expect(localStorage.setItem).toHaveBeenCalledTimes(2);
-      expect(localStorage.setItem).toHaveBeenLastCalledWith(CURRENT_THEME_KEY, 'dark');
+      expect(result.current.currentTheme).toBe('light');
     });
   });
 });
